c529a5fe42bbc10fdd93cba34b06e9be47fdb2bc
RadiantK <pps8853@gmail.com>
2022-03-07 12:33:29 +0900

    로그인 컨트롤러 구현
    로그인 컨트롤러를 구현하고 세션에 값을 담아줍니다.

---
+++ JSPBoard/src/main/java/com/radiantk/board/controller/LoginController.java
@@ -0,0 +1,76 @@
+package com.radiantk.board.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.radiantk.board.dao.UserDao;
+
+@SuppressWarnings("serial")
+@WebServlet("/board/login")
+public class LoginController extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String loginId = null;
+		if(request.getSession().getAttribute("sessionId") != null) {
+			loginId = (String) request.getSession().getAttribute("sessionId");
+		}
+		if(loginId != null) {
+			System.out.println("이미 로그인 된 아이디 입니다.");
+			response.sendRedirect("/board/main");
+		}
+		RequestDispatcher rd = request.getRequestDispatcher("/board/login.jsp");
+		rd.forward(request, response);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+
+		
+		
+		String userId_ = request.getParameter("userId");
+		String userId = "";
+		if(userId != null && !userId_.equals("")) userId = userId_;
+		
+		String password_ = request.getParameter("password");
+		String password = "";
+		if(password_ != null && !password_.equals("")) password = password_;
+		
+		UserDao ud = new UserDao();
+		
+		int result = ud.login(userId, password);
+		
+		if(result == 1) {
+			System.out.println("로그인 완료!");
+			HttpSession session = request.getSession();
+			session.setAttribute("sessionId", userId);
+			
+			response.sendRedirect("/board/main");
+		}
+		else if (result == 0) {
+			System.out.println("비밀번호가 일치하지 않습니다.");
+			response.sendRedirect("/board/login");
+
+		} 
+		else if(result == -1) {
+			System.out.println("데이터베이스 오류 입니다.");
+			response.sendRedirect("/board/login");
+
+		} 
+		else if(result == -2) {
+			System.out.println("비어있는 값이 있습니다..");
+			response.sendRedirect("/board/login");
+
+		}
+	}
+}
