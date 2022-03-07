87d2a6f60deb0eb80b1364c2d33e400929d84bfd
RadiantK <pps8853@gmail.com>
2022-03-07 12:35:21 +0900

    유저 엔티티
    DB의 레코드를 담을 유저 엔티티

---
+++ JSPBoard/src/main/java/com/radiantk/board/entity/User.java
@@ -0,0 +1,65 @@
+package com.radiantk.board.entity;
+
+import java.util.Date;
+
+public class User {
+
+	private String userId;
+	private String password;
+	private String userName;
+	private String email;
+	private Date regDate;
+	
+	public User() {}
+
+	public User(String userId, String password, String userName, String email) {
+		this.userId = userId;
+		this.password = password;
+		this.userName = userName;
+		this.email = email;
+	}
+
+
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	};
+	
+	
+}
