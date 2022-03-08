41b9a33e9200c0ea3cbb03d5c4cda544cd441e82
RadiantK <pps8853@gmail.com>
2022-03-08 15:18:15 +0900

    게시판 세부사항 페이지 컨트롤러
    게시판의 세부사항과 내용을 표시하는 페이지 컨트롤러를 생성

---
+++ JSPBoard/src/main/java/com/radiantk/board/controller/BoardViewController.java
@@ -0,0 +1,30 @@
+package com.radiantk.board.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.radiantk.board.dao.BoardDao;
+import com.radiantk.board.entity.Board;
+
+@SuppressWarnings("serial")
+@WebServlet("/board/view")
+public class BoardViewController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		int no = Integer.parseInt(request.getParameter("no"));
+		
+		BoardDao bd = new BoardDao();
+		Board board = bd.detailBoard(no);
+		request.setAttribute("board", board);
+		
+		request.getRequestDispatcher("/board/view.jsp").forward(request, response);
+	}
+}
