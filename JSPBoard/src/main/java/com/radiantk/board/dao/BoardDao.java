f5ceecebc015bdf4ce4653c0a67e6f8579cad756
RadiantK <pps8853@gmail.com>
2022-03-09 14:23:30 +0900

    BoardDao 삭제된 데이터 확인
    게시판글에서 삭제된 게시글을 구분하기 위해서 available 쿼리문 추가

--- JSPBoard/src/main/java/com/radiantk/board/dao/BoardDao.java
+++ JSPBoard/src/main/java/com/radiantk/board/dao/BoardDao.java
@@ -7,11 +7,11 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import com.radiantk.board.entity.Board;
+import com.radiantk.board.Entity.Board;
 import com.radiantk.board.util.DatabaseUtil;
 
 public class BoardDao {
-	// 전체 게시물 가져오기
+
 	public List<Board> getBoardList() {
 		return getBoardList("title", "", 1);
 	}
@@ -22,7 +22,7 @@ public class BoardDao {
 		
 		String sql = "SELECT * "
 				+ " FROM (SELECT ROW_NUMBER() OVER(ORDER BY regdate DESC) RN, "
-				+ " BOARD.* FROM BOARD WHERE "+type+" LIKE ?) B"
+				+ " BOARD.* FROM BOARD WHERE "+type+" LIKE ? AND available = 1) B"
 				+ " WHERE RN BETWEEN ? AND ?";
 		Connection con = null;
 		PreparedStatement st = null;	
@@ -43,8 +43,9 @@ public class BoardDao {
 				Date regDate = rs.getDate("regdate"); // 등록일
 				int hit = rs.getInt("hit"); // 조회수
 				String content = rs.getString("content"); //  상세내용
+				int available = rs.getInt("available");
 				
-				Board bo = new Board(bNo, title, writer, regDate, hit, content);
+				Board bo = new Board(bNo, title, writer, regDate, hit, content, available);
 				
 				board.add(bo);
 			}
@@ -88,8 +89,9 @@ public class BoardDao {
 				Date regDate = rs.getDate("regdate");
 				int hit = rs.getInt("hit");
 				String content = rs.getString("content");
+				int available = rs.getInt("available");
 				
-				board = new Board(bNo, title, writer, regDate, hit, content);
+				board = new Board(bNo, title, writer, regDate, hit, content, available);
 			}
 			
 		} catch(Exception e) {
@@ -108,12 +110,64 @@ public class BoardDao {
 		return board;
 	}
 	
+	public int updateBoard(String title, String content, int no) {
+		String sql = "UPDATE BOARD SET title = ?, content = ? WHERE BNO = ?";
+		
+		Connection con = null;
+		PreparedStatement st = null;
+		
+		try {
+			con = DatabaseUtil.getConnection();
+			st = con.prepareStatement(sql);
+			st.setString(1, title);
+			st.setString(2, content);
+			st.setInt(3, no);
+	
+			return st.executeUpdate();
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(st != null) st.close();
+			} catch (Exception e) {}
+			try {
+				if(con != null) con.close();
+			} catch (Exception e) {}
+		}
+		return -1; // db 오류
+	}
+	
+	public int deleteBoard(int no) {
+		String sql = "UPDATE BOARD SET available = 0 WHERE BNO = ?";
+		
+		Connection con = null;
+		PreparedStatement st = null;
+		
+		try {
+			con = DatabaseUtil.getConnection();
+			st = con.prepareStatement(sql);
+			st.setInt(1, no);
+	
+			return st.executeUpdate();
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(st != null) st.close();
+			} catch (Exception e) {}
+			try {
+				if(con != null) con.close();
+			} catch (Exception e) {}
+		}
+		return -1; // db 오류
+	}
+	
 	public int getBoardCount(String type, String searchWord) {
 		int count = 0;
 		
 		String sql = "SELECT COUNT(BNO) AS COUNT "
 				+ " FROM (SELECT ROW_NUMBER() OVER(ORDER BY regdate DESC) RN, "
-				+ " BOARD.* FROM BOARD WHERE "+type+" LIKE ?) B";
+				+ " BOARD.* FROM BOARD WHERE "+type+" LIKE ? AND available = 1) B";
 		Connection con = null;
 		PreparedStatement st = null;	
 		ResultSet rs = null;
